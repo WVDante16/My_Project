748bf9cc6380881c544aff17a7b05a783f41cc43
unknown <walbertozekrom@gmail.com>
2022-04-25 13:27:23 -0500

    Cambios funkys

--- components/Comp.js
+++ components/Comp.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StatusBar, Text, View} from 'react-native';
 import Box from './Box/Box';
 import styles from './styles';
+import Column from './Tools/Column';
+import Row from './Tools/Row';
 
 function Comp() {
     return (
-        <View style={styles.container}>
-            <Box>UwU</Box>
-            <Box>OwO</Box>
-            <Box>AwA</Box>
+        <View style = {styles.container}>
+            <StatusBar hidden = {true} />
+            <Row>
+                <Column>
+                    <Box>AwA</Box>
+                    <Box>EwE</Box>
+                    <Box>IwI</Box>
+                </Column>
+
+                <Column>
+                    <Box>OwO</Box>
+                    <Box>UwU</Box>
+                    <Box>UnU</Box>
+                </Column>
+            </Row>
         </View>
     );
 }
--- components/styles.js
+++ components/styles.js
@@ -3,28 +3,45 @@ import {Platform, StyleSheet, StatusBar} from "react-native";
 export default StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: 'ghostwhite',
         ...Platform.select({
-            ios: {paddingTop: 20},
+            ios: {paddingTop: 32},
             android: {paddingTop: StatusBar.currentHeight}
         })
     },
 
     box: {
-        width: 100,
-        //height: 100,
+        width: 80,
+        height: 80,
         justifyContent: 'center',
         alignItems: 'center',
-        alignSelf: 'stretch',
+        //alignSelf: 'stretch',
         borderWidth: 1,
         backgroundColor: 'lightgray',
+        margin: 10,
     },
 
     boxText: {
         color: "darkslategray",
         fontWeight: "bold",
-    }
+    },
+
+    row: {
+        flex: 1,
+        //alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignSelf: 'stretch',
+    },
+
+    column: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignSelf: 'stretch',
+    },
 });
